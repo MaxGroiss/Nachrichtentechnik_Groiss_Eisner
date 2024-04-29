17220c5713df3f6a5d3bda68a0ceedea4aff5c5c
MaxGroiss <163782362+MaxGroiss@users.noreply.github.com>
2024-04-29 20:50:31 +0200

    Anpassungen optischer Natur

--- Ass1.2.py
+++ Ass1.2.py
@@ -8,13 +8,6 @@ p_r = 2*pow(10, -12)
 n_0 = 4.047*pow(10, -21)
 B_1 = 2.46*pow(10, 6)
 B_2 = 120*pow(10, 3)
-plt.rc('font', family='serif')
-
-plt.rcParams.update({
-    "text.usetex": True,
-    "font.family": "sans-serif",
-    "font.sans-serif": "Helvetica",
-})
 
 
 def calculate_ebdn0(mode, b, p_r, n_0, db_list):
@@ -46,18 +39,30 @@ def calculate_ebdn0(mode, b, p_r, n_0, db_list):
             w_eff_list.append(np.divide(i, b))
     return np.array(eb_n0_list), np.array(w_eff_list)
 
+plt.rc('font', family='serif')
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "sans-serif",
+    "font.sans-serif": "Helvetica",
+})
+
+
 
 x = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 
 
 # plt.plot(x, get_C_values(B_1, n_0 , get_p_values(db_list, p_r)), 'b-', label=r'Transfer')
 plt.plot(x, calculate_ebdn0(True, B_1, p_r, n_0, db_list)[0], 'r-', label=r'$E_b/N_0$ in dB (S-band)')
-plt.plot(x, calculate_ebdn0(True, B_1, p_r, n_0, db_list)[1], 'b-', label=r'$W_eff$ in bits/s/Hz (S-band)')
-plt.plot(x, calculate_ebdn0(True, B_2, p_r, n_0, db_list)[0], 'y-', label=r'$E_b/N_0$ in dB (VHF)')
-plt.plot(x, calculate_ebdn0(True, B_2, p_r, n_0, db_list)[1], 'g-', label=r'$W_eff$ in bits/s/Hz (VHF)')
+plt.plot(x, calculate_ebdn0(True, B_1, p_r, n_0, db_list)[1], 'r--', label=r'$W_eff$ in bits/s/Hz (S-band)')
+plt.plot(x, calculate_ebdn0(True, B_2, p_r, n_0, db_list)[0], 'g-', label=r'$E_b/N_0$ in dB (VHF)')
+plt.plot(x, calculate_ebdn0(True, B_2, p_r, n_0, db_list)[1], 'g--', label=r'$W_eff$ in bits/s/Hz (VHF)')
+plt.axhline(y=1, color='y', linestyle='-', label="1 bits/s/Hz")
 plt.title(r'Transferverhalten über die Wochentage')
-plt.ylabel(r'$C$ in bit/s')
-plt.xlabel(r'Days')
+plt.ylabel(r'$E_b/N_0$ in dB und $W_eff$ in bits/s/Hz')
+plt.xlabel(r'Weekdays')
 plt.legend(fontsize=8)
+plt.xlim(0,6)
 plt.grid()
 plt.show()
+
